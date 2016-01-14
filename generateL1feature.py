c5f393eb615bd803f445744b1d2268380ae16a82
xin chen <cuc_cx@hotmail.com>
2016-01-14 10:48:32 +0800

    change dir of generate files
    TODO: learn how to use macro of reletive address to avoid this problem

--- generateL1feature.py
+++ generateL1feature.py
@@ -5,5 +5,5 @@ from GenerateNgram import *
 
 
 lable,Query,Title,Query_Len,Query_Freq,Query_First,Query_Last = ReadTraining_feature()
-#Trainngram("C:\\Users\\v-chexi\\Documents\\CIKM\\Featureusedfortest\\Query_pig_train.txt",True,"C:\\Users\\v-chexi\\Documents\\CIKM\\abstractfeatures\\Query_word_vector")
-Trainngram("C:\\Users\\v-chexi\\Documents\\CIKM\\Featureusedfortest\\Title_pig_train.txt",False,"C:\\Users\\v-chexi\\Documents\\CIKM\\abstractfeatures\\Title_word_vector")
+Trainngram("D:\\Featureusedfortest\\Query_pig_train.txt",True,"D:\\abstractfeatures\\Query_word_vector")
+Trainngram("D:\\Featureusedfortest\\Title_pig_train.txt",False,"D:\\abstractfeatures\\Title_word_vector")
